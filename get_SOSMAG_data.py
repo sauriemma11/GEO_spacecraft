c5932fc8d440d5452c655e5cde2a2d34771ddbbd
sauriemma11 <111297438+sauriemma11@users.noreply.github.com>
2023-03-09 11:58:55 -0600

    Update get_SOSMAG_data.py
    Can now retrieve data for a timespan > 1 day

--- get_SOSMAG_data.py
+++ get_SOSMAG_data.py
@@ -1,16 +1,22 @@
 import pytplot
 from pyspedas import sosmag_load
+from datetime import datetime, timedelta
 
-trange = ['2023-02-23 00:00:00', '2023-02-23 23:59:00']
-file_name = 'SOSMAG_20230223_' 
-save_to_dir = '/path/to/data/directory'
+start_date_str = '2022-12-07 00:00:00'
+end_date_str = '2022-12-14 23:59:00'
+start_date = datetime.strptime(start_date_str, '%Y-%m-%d %H:%M:%S')
+end_date = datetime.strptime(end_date_str, '%Y-%m-%d %H:%M:%S')
+delta = timedelta(days=1)
 
-# tplot_ascii() taken from pytplot;
-# Copyright 2018 Regents of the University of Colorado. All Rights Reserved.
-# Released under the MIT license.
-# This software was developed at the University of Colorado's Laboratory for Atmospheric and Space Physics.
-# Verify current version before use at: https://github.com/MAVENSDC/Pytplot
+save_to_dir = 'C:/Users/Sarah/PycharmProjects/pythonProject1/sosmag_data/'
+
 def tplot_ascii(tvar, filename=None, extension='.csv'):
+    # tplot_ascii() taken from pytplot;
+    # Copyright 2018 Regents of the University of Colorado. All Rights Reserved.
+    # Released under the MIT license.
+    # This software was developed at the University of Colorado's Laboratory for Atmospheric and Space Physics.
+    # Verify current version before use at: https://github.com/MAVENSDC/Pytplot
+    
     # grab data, prepend index column
     if filename == None:
         filename = tvar
@@ -19,19 +25,24 @@ def tplot_ascii(tvar, filename=None, extension='.csv'):
     # only try to save spec_bins (y-values in spectrograms) if we're sure they exist
     if 'spec_bins' in pytplot.data_quants[tvar].coords.keys():
         pytplot.data_quants[tvar].coords['spec_bins'].to_pandas().to_csv(filename + '_v' + extension)
-
-
-# load data with the given time range
-sosmag_load(trange=trange, datatype='1m')
+    print(f"File {filename+extension} saved successfully.")
 
 user_input = input("Select variable to save as CSV. Enter 'gse', 'hpen', or 'pos': ")
-if user_input.lower() == 'hpen':
-    tplot_ascii(tvar='sosmag_1m_b_hpen', filename=(save_to_dir+file_name+'hpen'))
-elif user_input.lower() == 'gse':
-    tplot_ascii(tvar='sosmag_1m_b_gse', filename=(save_to_dir+file_name+'gse'))
-elif user_input.lower() == 'pos':
-    tplot_ascii(tvar='sosmag_1m_position', filename=(save_to_dir+file_name+'pos'))
-else:
-    print("SOSMAG data not saved to local directory.")
-
-
+while start_date <= end_date:
+    trange = [start_date.strftime('%Y-%m-%d %H:%M:%S'), (start_date + delta).strftime('%Y-%m-%d %H:%M:%S')]
+    file_name = 'SOSMAG_' + start_date.strftime('%Y%m%d') + '_'
+    sosmag_load(trange=trange, datatype='1m')
+    if user_input.lower() == 'hpen':
+        tvar = 'sosmag_1m_b_hpen'
+        filename = (save_to_dir+file_name+'hpen')
+    elif user_input.lower() == 'gse':
+        tvar = 'sosmag_1m_b_gse'
+        filename = (save_to_dir+file_name+'gse')
+    elif user_input.lower() == 'pos':
+        tvar = 'sosmag_1m_position'
+        filename = (save_to_dir+file_name+'pos')
+    else:
+        print("SOSMAG data not saved to local directory.")
+        continue
+    tplot_ascii(tvar=tvar, filename=filename)
+    start_date += delta
